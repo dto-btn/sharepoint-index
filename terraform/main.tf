1ef314ff79720ae5aa0a82ffbd545e7a882daf57
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-10-23 10:24:38 -0400

    wip: added tf for basic logic app

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+/****************************************************
+*                       RG                          *
+*****************************************************/
+resource "azurerm_resource_group" "main" {
+  name     = "${var.name_prefix}${var.project_name}-rg"
+  location = var.default_location
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "${replace(var.project_name, "_", "")}sto"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "${var.name_prefix}${var.project_name}-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name = "WS1"
+  os_type = "Windows"
+
+}
+
+resource "azurerm_logic_app_standard" "main" {
+  name                       = "${replace(var.project_name, "_", "-")}-logic-app"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  app_service_plan_id        = azurerm_service_plan.main.id
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+  version = "~4"
+}
---
+++ terraform/terraform.tf
@@ -0,0 +1,23 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "ScSc-CIO_ECT_Infrastructure-rg"
+    storage_account_name = "ectinfra"
+    container_name       = "tfstate"
+    key                  = "sharepoint-index.terraform.tfstate"
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f5fb90f1-6d1e-4a21-8935-6968d811afd8"
+}
