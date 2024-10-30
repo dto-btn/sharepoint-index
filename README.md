84771bf52398a357757a2e4700591e01e43a6586
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-10-30 14:56:51 -0400

    wip: durable function

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Pre-requisite:
 * Install recommended extensions in VS Code (such as `pylint`, etc.)
 * Install [Azure Function Core Tools](https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=linux%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-python#install-the-azure-functions-core-tools)
 
+Create your python env: 
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+
 ### Az CLI
 
 In order to run this project locally you need Az CLI installed and to be logged in the sub you will be using the
@@ -23,7 +29,7 @@ az login --scope https://graph.microsoft.com/.default
 
 ### Running the Az Function
 
-Ensure you have a `local.settings.json` file present inside the SharepointIndexer folder that contains:
+Ensure you have a `local.settings.json` file present inside the `sharepoint_indexer` folder that contains:
 
 ```json
 {
@@ -41,6 +47,6 @@ Ensure you have a `local.settings.json` file present inside the SharepointIndexe
 This Azure Function was init via the Azure Core Tools: 
 
 ```bash
-func init SharepointIndexer --worker-runtime python --model V2
-func new --template "Http Trigger" --name IndexSharepointSiteFiles
+func init sharepoint_indexer --worker-runtime python --model V2
+func new --template "Http Trigger" --name index_sharepoint_site_files
 ```
--- sharepoint_indexer/function_app.py
+++ sharepoint_indexer/function_app.py
@@ -1,15 +1,15 @@
-import asyncio
-import datetime
-import json
 import logging
 import os
 
 import azure.functions as func
+import azure.durable_functions as df
 import requests
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from azure.durable_functions import DurableOrchestrationClient
 from msgraph import GraphServiceClient
 
-app = func.FunctionApp()
+#app = func.FunctionApp()
+app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -20,25 +20,76 @@ _credential = DefaultAzureCredential()
 _bearer_token_provider = get_bearer_token_provider(_credential,
                                                    "https://graph.microsoft.com/.default")
 _graph_client = GraphServiceClient(DefaultAzureCredential(), _scopes)
+
 _domain = os.getenv("SHAREPOINT_DOMAIN", "163gc.sharepoint.com")
 
-async def get_site_info(path):
+# Durable function that fetches all sscplus page IDs/pages and uploads them
+# @app.route(route="orchestrators/{functionName}")
+# @app.durable_client_input(client_name="client")
+# async def http_start(req: func.HttpRequest, client):
+#     function_name = req.route_params.get('functionName')
+#     instance_id = await client.start_new(function_name)
+#     response = client.create_check_status_response(req, instance_id)
+#     return response
+
+# # timer triggered function to fetch index data, runs Sat at midnight
+# # cron with 6 args, the first one being seconds.
+# @app.schedule(schedule="0 0 4 * * Fri", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+# @app.durable_client_input(client_name="client")
+# async def fetch_index_timer_trigger(myTimer: func.TimerRequest, client) -> None:
+#     instance_id = await client.start_new("fetch_index_data")
+#     logging.info("fetch index timer trigger function executed")
+
+@app.route(route="index_sharepoint_site_files", auth_level=func.AuthLevel.FUNCTION)
+@app.durable_client_input(client_name="client")
+async def index_sharepoint_site_files(req: func.HttpRequest, client: DurableOrchestrationClient) -> func.HttpResponse:
+    """
+    Main trigger method that reads site files and index them into an Azure Search Service index
+
+    Debug with 'F5' (if you have Azure Function Extension installed, or you can also Ctrl+Shift+P and 
+    -> Attach: project_name function)
+    """
+    logging.info('Python HTTP trigger function processed a request.')
+
+    site_name = req.params.get('site_name')
+    lib_name = req.params.get('lib_name')
+    if not site_name or not lib_name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            site_name = req_body.get('site_name')
+            lib_name = req_body.get('lib_name')
+
+    if site_name and lib_name:
+        instance_id = await client.start_new("start", site_name, lib_name)
+        return client.create_check_status_response(req, instance_id)
+    else:
+        return func.HttpResponse(body="Unable to start durable function due to missing parameters", status_code=400)
+
+async def start(site_name: str, lib_name: str):
+    """
+    Initiate the whole process of loading up a site, fetching site items id and then indexing each one of them.
+    """
+    logging.info('Inside Start function of durable method')
+
+async def _get_site_info(path):
     """Get Sharepoint site info based on the name"""
     result = await _graph_client.sites.by_site_id(f'{_domain}/:/sites/{path}').get()
-    _id = result.id
     return result
 
-async def get_site_page(site):
+async def _get_site_page(site):
     """TODO: Test this method to see what it yields."""
     _id = site.id.split(',')[1]
-    print(f"The id used to retreive pages: {_id}")
+    logger.debug("The id used to retreive pages: %s", _id)
     result = await _graph_client.sites.by_site_id(_id).pages.get()
     return result
 
-async def get_site_drive(site, drive_name):
+async def _get_site_drive(site, drive_name):
     """Method used to pass a site and a drive name in order to return the Drive instance"""
     _id = site.id.split(',')[1]
-    print(f"The id used to retreive pages: {_id}")
+    logger.debug("The id used to retreive pages: %s", _id)
     # get drives https://graph.microsoft.com/v1.0/sites/{siteid}/drives
     result = await _graph_client.sites.by_site_id(_id).drives.get()
     filtered_drives = [drive for drive in result.value if drive.odata_type== "#microsoft.graph.drive"]
@@ -47,37 +98,28 @@ async def get_site_drive(site, drive_name):
             return drive
     return None
 
-async def main():
-    """TODO: Convert this to an Azure Function entry point"""
-    response = await get_site_info("DigitalTransformationProcessImprovement")
-    drive = await get_site_drive(response, "Documents")
-    get_files_id(drive.id)
-
-def get_files_id(drive_id: str):
+def _get_files_id(drive_id: str):
     """
     Get files id for each of the item within the drive passed in parameter.
     Will also download the files directly to FS
     """
     headers = {
-            'Accept': 'application/json'
+        'Accept': 'application/json'
+        "Bearer " + _bearer_token_provider()
     }
-    token = _bearer_token_provider()
-    headers["Authorization"] = "Bearer " + token
     # Can also use odata filter -> ?$select=id,name,@microsoft.graph.downloadUrl"
     url = f"https://graph.microsoft.com/v1.0/drives/{drive_id}/root/children"
-    print(f"Trying to get drive items from --> {url}")
+    logger.debug("Trying to get drive items from --> %s", url)
     r = requests.get(url, headers=headers, timeout=10)
     files = r.json()['value']
     for file in files:
         if '@microsoft.graph.downloadUrl' in file:
-            download_file(file['@microsoft.graph.downloadUrl'], file['name'])
+            _download_file(file['@microsoft.graph.downloadUrl'], file['name'])
 
-def download_file(url, name):
+def _download_file(url, name):
     """Download file to filesystem"""
     with requests.get(url, stream=True, timeout=10) as r:
         r.raise_for_status()
         with open('./temp_download/' + name, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
-
-asyncio.run(main())
